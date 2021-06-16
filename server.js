03d2d649775fd7a9f3848f17cf13f74edb69e0e0
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-06-16 20:11:12 +0300

    movies list

--- server.js
+++ server.js
@@ -65,8 +65,11 @@ class Movie {
   constructor(movieData) {
     this.title = movieData.original_title;
     this.overview = movieData.overview;
-    this.poster = 'http://image.tmdb.org/t/p/w342' + movieData.poster_path;
-    this.rating = movieData.vote_average;
+    this.average_votes = movieData.vote_average;
+    this.total_votes = movieData.vote_count;
+    this.image_url = 'http://image.tmdb.org/t/p/w342' + movieData.poster_path;
+    this.popularity = movieData.popularity;
+    this.released_on = movieData.release_date;
   }
 }
 
