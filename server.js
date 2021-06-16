712bea33f95ed81ae2a5dfc6769f22637b4a7891
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-06-16 08:25:31 +0300

    adding class movies

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const weatherData = require('./data/weather.json');
 const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT;
+// const MOVIE_DB_KEY = process.env.MOVIE_DB_KEY;
 
 app.use(cors()); // after you initialize your express app instance
 // a server endpoint
@@ -27,5 +28,20 @@ class Weather {
     this.date = weatherData.valid_date;
   }
 }
+// app.get('/movies', (req, res) => {
+//   const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_DB_KEY}&query=${req.query.query}`;
+//   superagent.get(movieUrl).then( movieRes => {
+//     const moviesList = movieRes.body.results.map( data => new Movie(data));
+//     res.send(moviesList)
+//   }).catch(error => {console.error});
+// });
 
+// class Movie{
+//   constructor(data){
+//     this.title = data.original_title;
+//     this.overview = data.overview;
+//     this.poster = 'http://image.tmdb.org/t/p/w342' + data.poster_path;
+//     this.rating = data.vote_average
+//   }
+// }
 app.listen(PORT); // kick start the express server to work.
